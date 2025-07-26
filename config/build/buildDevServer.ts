5f933a846dbb146bb2890e7d98d016c3681ec858
Алексей Кузьмичев <kuzmichev.alexey@mpstats.io>
2025-07-26 17:12:57 +0300

    feat/[webpack]_add prod and dev env

---
+++ config/build/buildDevServer.ts
@@ -0,0 +1,13 @@
+import { Configuration as WebpackServerConfiguration } from "webpack-dev-server"
+import { IWebpackConfigOptions } from './types/config';
+
+export const buildDevServer = (options: IWebpackConfigOptions): WebpackServerConfiguration => {
+    const { port } = options
+    return  {
+        port,
+        static: {
+            directory: options.paths.public,
+        },
+        open: true,
+    }
+}
