c8838d13156626718d2c73be5b4f6fed2865eaa1
Алексей Кузьмичев <kuzmichev.alexey@mpstats.io>
2025-08-10 21:30:48 +0300

    feat/[testing] add react tests for react components

--- jest.config.ts
+++ jest.config.ts
@@ -1,7 +1,12 @@
 export default {
     rootDir: './',
     clearMocks: true,
-    'moduleNameMapper': {
+    testEnvironment: 'jsdom',
+    moduleNameMapper: {
         '^@src(.*)$': '<rootDir>/src$1',
-    }
+        '^.+\\.(css|less|scss)$': 'babel-jest'
+    },
+    testPathIgnorePatterns: ['<rootDir>/node_modules', '<rootDir>/dist'],
+    setupFilesAfterEnv: ['<rootDir>src/jestSetup.ts'],
+    moduleDirectories: ['<rootDir>/node_modules', '<rootDir>/src'],
 }
